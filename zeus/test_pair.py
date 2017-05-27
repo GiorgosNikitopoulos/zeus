4ddb09cde192dbaf73b80d8141d12fe9bb744860
GiorgosNikitopoulos <gnikitopoulos@inf.uth.gr>
2017-05-27 18:01:06 +0300

    Add comments and Refactor code

--- zeus/test_pair.py
+++ zeus/test_pair.py
@@ -5,30 +5,24 @@ from Crypto import Random
 import binascii
 
 def main():
-    Resoolt3 = 0
-    Resoolt1 = [None] * 5
-    Resoolt2 = [None] * 5
-    key = ElGamal.generate(256, Random.new().read)
-    q = (key.p - 1) / 2
+    Result3 = 0
+    Result1 = [None] * 5##Init Result1
+    Result2 = [None] * 5##Init Result2
+    key = ElGamal.generate(256, Random.new().read) ##Generate Elgamal key
+    q = (key.p - 1) / 2 ##Get order
     alpha = [None] * 5
-    beta = [None] * 5
-    messages = [None] * 5
+    beta = [None] * 5 ##Init ElGamal Pair
+    messages = [None] * 5 ##Init message pair
     messages = ['Text_for_one', 'Text_for_two', 'Text_for_three', 'Text_for_four', 'Text_for_five']
     for i in range(5):
-        alpha[i], beta[i] = key.encrypt(messages[i], key.y)
+        alpha[i], beta[i] = key.encrypt(messages[i], key.y) ##Encrypt messages
     for i in range(5):
         alpha[i] = int(binascii.hexlify(alpha[i]), 16)
-        beta[i] = int(binascii.hexlify(beta[i]), 16)
-    pairshuffle_obj = PairShuffle(key.p, 5)
-    print key.p
-    print q
-    print key.g
-    print key.y
-    print alpha
-    print beta
-    Resoolt1, Resoolt2, b_var = pairshuffle_obj.go_shuffle_shuffle(key.p, q, key.g, key.y, alpha, beta)
-    print Resoolt1
-    print Resoolt2
+        beta[i] = int(binascii.hexlify(beta[i]), 16) ##Turn the messages into ints
+    pairshuffle_obj = PairShuffle(key.p, 5) ##Init PairShuffle object
+    Result1, Result2, b_var = pairshuffle_obj.go_shuffle_shuffle(key.p, q, key.g, key.y, alpha, beta)
+    print Result1
+    print Result2
     print b_var
 
 if __name__ == "__main__":
